d288621b00e03980d0d9aff5bd6ec14a9cca5da9
alvar91 <aleksey91scorp@bk.ru>
2024-12-24 01:27:21 +0300

    docs: Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,22 @@ package org.example;
 
 import org.example.controllers.ShortLinkController;
 
+/**
+ * The entry point of the application.
+ * <p>
+ * This class initializes and runs the {@link ShortLinkController}, which handles
+ * user interactions and application operations.
+ * </p>
+ *
+ * @author alvar91
+ * @version 1.0
+ */
 public class Main {
+    /**
+     * The main method, serving as the application entry point.
+     *
+     * @param args Command-line arguments passed during application startup.
+     */
     public static void main(String[] args) {
         // Create an instance of the ShortLinkController
         ShortLinkController shortLinkController = new ShortLinkController();
